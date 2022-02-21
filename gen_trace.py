5033668759797aa935e57e04314705b48d825108
richardlee <18626685+richardlee159@users.noreply.github.com>
2022-02-21 13:53:16 +0800

    Update gen_trace.py

--- gen_trace.py
+++ gen_trace.py
@@ -1,24 +1,30 @@
 from numpy import random
-import sys
+import argparse
+
+dict_gen_next = {
+    'const': lambda x: x,
+    'uniform': lambda x: random.uniform(0, 2 * x),
+    'exp': lambda x: random.exponential(x),
+}
+
+parser = argparse.ArgumentParser(description='Generate tracefile for rload.')
+parser.add_argument('-D', '--dist', choices=dict_gen_next.keys(),
+                    help='distribution of the inter-arrival time of requests')
+parser.add_argument('-d', '--duration', type=int, required=True,
+                    help='duration of test in seconds')
+parser.add_argument('-R', '--rate', type=int, required=True,
+                    help='work rate in requests/second')
+args = parser.parse_args()
 
 # unit of time: ms
-distribution = sys.argv[1]
-duration = 5 * 1000
-rate = 100
+distribution = args.dist
+duration = args.duration * 1000
+rate = args.rate
 interval = 1000 / rate
-
-
-def gen_next():
-    if distribution == 'const':
-        return interval
-    elif distribution == 'uniform':
-        return random.uniform(0, 2 * interval)
-    elif distribution == 'exp':
-        return random.exponential(interval)
-
+gen_next = dict_gen_next[distribution]
 
 start = 0
 with open('trace.txt', 'w') as f:
     while start < duration:
         f.write(f'{start:.0f}\n')
-        start += gen_next()
+        start += gen_next(interval)
