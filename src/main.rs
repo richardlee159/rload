6158d9cab07e615be22a69388730aef03aa249a9
richardlee <18626685+richardlee159@users.noreply.github.com>
2024-02-26 12:42:01 +0800

    Finish URL part

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ mod workload;
 #[macro_use]
 extern crate log;
 
-use clap::{ArgGroup, Parser};
-use reqwest::{Client, StatusCode, Url};
+use clap::{ArgGroup, Parser, ValueEnum};
+use reqwest::{Client, StatusCode};
 use std::{fs::File, io::Write, path::PathBuf, time::Duration};
 use tokio::{
     runtime::Builder,
@@ -17,10 +17,19 @@ use crate::workload::matmul;
 
 type Result<T> = core::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
+#[derive(Clone, Copy, ValueEnum)]
+enum RequestFormat {
+    Matmul,
+    Compute,
+    Io,
+}
+
 #[derive(Parser)]
 #[clap(version)]
 #[clap(group(ArgGroup::new("generator").required(true).args(&["duration", "tracefile"])))]
 struct Args {
+    #[clap(long)]
+    ip: String,
     #[clap(short = 'f', long)]
     tracefile: Option<PathBuf>,
     #[clap(short, long, requires = "rate", help = "Duration of test (s)")]
@@ -31,7 +40,52 @@ struct Args {
     timeout: u64,
     #[clap(long)]
     results_path: Option<PathBuf>,
-    url: Url,
+    #[clap(long)]
+    request_format: RequestFormat,
+    #[clap(long)]
+    input_size: u64,
+    #[clap(long)]
+    hot_percent: f64,
+}
+
+struct UrlGenerator {
+    hot_url: String,
+    cold_url: String,
+    hot_percent: f64,
+    request_counter: f64,
+}
+
+impl UrlGenerator {
+    fn new(ip: &str, hot_percent: f64) -> Self {
+        assert!(hot_percent >= 0.0);
+        assert!(hot_percent <= 1.0);
+        let base_url = format!("http://{}:{}", ip, 8080);
+        Self {
+            hot_url: format!("{}/hot", base_url),
+            cold_url: format!("{}/cold", base_url),
+            hot_percent,
+            request_counter: rand::random(),
+        }
+    }
+
+    fn next(&mut self, request_format: RequestFormat) -> String {
+        self.request_counter += self.hot_percent;
+        let url = if self.request_counter >= 1.0 {
+            self.request_counter -= 1.0;
+            &self.hot_url
+        } else {
+            &self.cold_url
+        };
+        format!(
+            "{}/{}",
+            url,
+            match request_format {
+                RequestFormat::Matmul => "matmul",
+                RequestFormat::Compute => "compute",
+                RequestFormat::Io => "io",
+            }
+        )
+    }
 }
 
 #[derive(Debug)]
@@ -107,6 +161,8 @@ fn main() -> Result<()> {
 
 // #[tokio::main]
 async fn tokio_main(args: Args) -> Result<()> {
+    let mut url_gen = UrlGenerator::new(&args.ip, args.hot_percent);
+
     let client = Client::builder()
         .timeout(Duration::from_millis(args.timeout))
         .build()
@@ -126,8 +182,13 @@ async fn tokio_main(args: Args) -> Result<()> {
     let base = Instant::now();
     tokio::spawn(async move {
         for start in starts {
-            let url = args.url.to_string();
-            let request = client.post(&url).body(matmul(1));
+            let url = url_gen.next(args.request_format);
+            let request = client.post(&url).body(match args.request_format {
+                RequestFormat::Matmul => matmul(args.input_size),
+                RequestFormat::Compute => vec![],
+                RequestFormat::Io => vec![],
+            });
+
             let tx = tx.clone();
             time::sleep_until(base + start).await;
             tokio::spawn(async move {
