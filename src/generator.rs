0b1c83819be670b95173fa32c19696f8c5bf6a38
richardlee <18626685+richardlee159@users.noreply.github.com>
2024-02-28 21:16:40 +0800

    Remove exp-distr generator

--- src/generator.rs
+++ src/generator.rs
@@ -1,4 +1,3 @@
-use rand_distr::{Distribution, Exp};
 use std::time::Duration;
 
 pub struct ConstGen {
@@ -34,17 +33,4 @@ impl Iterator for ConstGen {
             None
         }
     }
-}
-
-#[allow(unused)]
-pub fn new_exp(duration: Duration, rate: u64) -> Vec<Duration> {
-    let dist = Exp::new(rate as f64).unwrap();
-    dist.sample_iter(rand::thread_rng())
-        .map(Duration::from_secs_f64)
-        .scan(Duration::ZERO, |t, iat| {
-            *t += iat;
-            Some(*t)
-        })
-        .take_while(move |&t| t < duration)
-        .collect()
 }
